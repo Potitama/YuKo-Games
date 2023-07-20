b04d696e7bf8658c9fc189bba5890818c7715c2d
Potitama <111692445+Potitama@users.noreply.github.com>
2023-07-20 10:12:08 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -240,3 +240,20 @@ $(window).scroll(function() {
       }
   });
 });
+
+$(window).scroll(function (){
+  fadeAnime();
+});
+
+function fadeAnime(){
+  $('.fade-up-area').each(function(){ 
+    var elemPos = $(this).offset().top-50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('blur');
+    }else{
+    $(this).removeClass('blur');
+    }
+    });
+}
